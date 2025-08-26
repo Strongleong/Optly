cdc4d0406504a451b67cfe4aa7e70937197a12a0
Nikita Chulkov <nikita_chul@mail.ru>
2025-08-26 18:57:08 +1100

    Docs and refactor

--- lib/cli.h
+++ lib/cli.h
@@ -6,21 +6,24 @@
         #define CLIARGS_IMPLEMENTATION
         #include "cliargs.h"
 
-    Commands:
-        Commands are CLI argumets // TODO: TBD
+    OVERVIEW:
+        This library provides a simple command-line argument parser with support
+        for commands and flags. Flags can be long-form (--example) or short-form (-e).
+        Short-form flags can be batched (e.g., `-abc` = `-a -b -c`).
+        Flags can also take values, either separated by space or `=` sign:
+            ./app --name=John
+            ./app -n John
 
-    Flags:
-        Flags are CLI arguments that starts with `-`. They can be long form `--example`
-        or short form `-s`. Short from flags can be "batched" (`-abcd` is same as `-a -b -c -d`)
-        Flags can have parameters that separated from flag with space or equeal sign `=`.
-        Batched flags can not have arguments. If argument does not takes argumet then actual value
-        of parsed flag is TRUE if flag was found.
-        Examples:
-            `./app --help`
-            `./app --url https://example.com`
-            `./app -j=12`
-            `./app -lah`
+        Commands are positional arguments that do not start with `-`.
+        A command can have its own flags.
 
+    Example:
+        `./app --verbose build --target x86`
+
+        In this example:
+            * Global flag: --verbose
+            * Command: build
+            * Command flag: --target x86
 */
 
 #ifndef CLIARGS_H
@@ -29,41 +32,62 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-// if you need more than 512 args for ONE command you should look in the mirror really deep
-#define COMMAND_FLAGS_MAX_COUNT 512
-#define MAX_PATH_LENGTH 4096
+#ifndef CLIDEF
+#define CLIDEF
+#endif // CLIDEF
 
+// if you need more than 512 args (per ONE command) you should look in the mirror really deep
+#define MAX_FLAGS_LENGTH 512
+#define MAX_PATH_LENGTH  4096
+
+/**
+ * Represents a single CLI flag.
+ * If `value` is NULL, it means the flag is present (boolean true).
+ */
 typedef struct {
-    char *fullname;
-    char shortname;
-    char *value;  // If NULL → treated as boolean TRUE
+    char *fullname;   ///< Long name of the flag (e.g., "verbose")
+    char  shortname;  ///< Short name (e.g., 'v')
+    char *value;      ///< Optional value for the flag
 } CliFlag;
 
+#define NULL_FLAG { NULL, 0, NULL }
+
+/**
+ * Represents a CLI command with associated flags.
+ */
 typedef struct {
-    char *name;
-    CliFlag *flags;  // NULL-terminated array of flags
+    char     *name;                       ///< Command name
+    CliFlag  *flags[MAX_FLAGS_LENGTH];    ///< Array of flags termitaed by NULL_FALG macro
 } CliCommand;
 
+#define NULL_COMMAND { NULL,  NULL_FLAG  }
+
+/**
+ * Represents parsed CLI arguments (binary name, command, flags).
+ */
 typedef struct {
-    char bin_name[MAX_PATH_LENGTH];
-    CliCommand *command;
-    CliFlag **parsed_flags; // dynamically allocated array of pointers to flags
-    size_t parsed_flags_count;
+    char      bin_name[MAX_PATH_LENGTH];           ///< Name of the executable
+    CliCommand *command;                           ///< Detected command
+    CliFlag   *parsed_flags[MAX_FLAGS_LENGTH];     ///< Array of parsed flags termitaed by NULL_COMMAND macro
+    size_t     parsed_flags_count;                 ///< Count of parsed flags
 } CliArgs;
 
-// Public API
-CliArgs *cli_parse_args(int argc, char *argv[], CliFlag *global_flags, CliCommand *commands);
-void usage(char const *const bin_name, CliCommand *commands, CliFlag *flags);
-void command_usage(char const *const bin_name, CliCommand *command);
 
-// Internal helpers (inline)
-static inline bool cli_is_flag_null(const CliFlag *flag) {
-    return flag == NULL || (flag->fullname == NULL && flag->shortname == 0);
-}
+/**
+ * Parse CLI arguments into a CliArgs structure.
+ */
+CLIDEF CliArgs cli_parse_args(int argc, char *argv[], CliFlag *flags, CliCommand *commands);
 
-static inline bool cli_is_command_null(const CliCommand *cmd) {
-    return cmd == NULL || (cmd->name == NULL && (cmd->flags == NULL || cli_is_flag_null(cmd->flags)));
-}
+/**
+ * Print usage message with commands and global flags.
+ */
+CLIDEF void cli_usage(char const *const bin_name, CliCommand *commands, CliFlag *flags);
+
+
+/**
+ * Print usage message for a specific command.
+ */
+CLIDEF void cli_command_usage(char const *const bin_name, CliCommand *command);
 
 #endif // CLIARGS_H
 
@@ -72,87 +96,128 @@ static inline bool cli_is_command_null(const CliCommand *cmd) {
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <libgen.h>
 
 #define SHIFT_ARG(argv, argc) (++(argv), --(argc))
 
-void usage_commands_list(CliCommand *commands) {
-  fprintf(stderr, "COMMANDS\n");
-
-  for (CliCommand *command = commands; !cli_is_command_null(command); command++) {
-    fprintf(stderr, "  %s\n", command->name);
-  }
+static CLIDEF bool cli_is_flag_null(const CliFlag *flag) {
+    return flag == NULL || (flag->fullname == NULL && flag->shortname == 0);
 }
 
-static void usage_flags(CliFlag *flags) {
-  fprintf(stderr, "GLOBAL FLAGS\n");
-
-  for (CliFlag *flag = flags; !cli_is_flag_null(flag); flag++) {
-    fprintf(stderr, "  -%c --%s\n", flag->shortname, flag->fullname);
-  }
+static CLIDEF bool cli_is_command_null(const CliCommand *cmd) {
+    return cmd == NULL || (cmd->name == NULL && (*cmd->flags == NULL || cli_is_flag_null(*cmd->flags)));
 }
 
-void command_usage(char const *const bin_name, CliCommand *command) {
-  fprintf(stderr, "%s [GLOBAL FLAGS] %s [COMMAND FLAGS]\n", bin_name, command->name);
-  usage_flags(command->flags);
+
+/**
+ * Print list of available commands.
+ */
+static CLIDEF void cli__usage_commands_list(CliCommand *commands) {
+    fprintf(stderr, "COMMANDS\n");
+
+    for (CliCommand *cmd = commands; !cli_is_command_null(cmd); cmd++) {
+        fprintf(stderr, "  %s\n", cmd->name);
+    }
 }
 
-void usage(char const *const bin_name, CliCommand *commands, CliFlag *flags) {
+/**
+ * Print list of flags.
+ */
+static CLIDEF void cli__usage_flags(CliFlag **flags) {
+    fprintf(stderr, "FLAGS\n");
+
+    for (CliFlag *flag = *flags; !cli_is_flag_null(flag); flag++) {
+        fprintf(stderr, "  -%c --%s\n", flag->shortname, flag->fullname);
+    }
+}
+
+CLIDEF void cli_command_usage(char const *const bin_name, CliCommand *command) {
+  char name[strlen(bin_name) + 1];
+  strcpy(name, bin_name);
+  fprintf(stderr, "%s [GLOBAL FLAGS] %s [COMMAND FLAGS]\n", basename(name), command->name);
+  cli__usage_flags(command->flags);
+}
+
+CLIDEF void cli_usage(char const *const bin_name, CliCommand *commands, CliFlag *flags) {
   fprintf(stderr, "%s [GLOBAL FLAGS] <COMMAND> [COMMAND FLAGS]\n", bin_name);
-  usage_commands_list(commands);
-  usage_flags(flags);
+  cli__usage_commands_list(commands);
+  cli__usage_flags(&flags);
 }
 
-static bool cli_flag_matches(const char *arg, const CliFlag *flag) {
+/**
+ * Check if argument matches a flag definition.
+ */
+static CLIDEF bool cli__flag_matches(const char *arg, const CliFlag *flag) {
     bool is_short = arg[1] != '-';
+
     return (!is_short && strcmp(arg + 2, flag->fullname) == 0) ||
            (is_short && (arg[1] == flag->shortname));
 }
 
-static CliFlag *cli_find_flag(const char *arg, CliFlag *flags) {
+/**
+ * Find a flag by argument.
+ */
+static CLIDEF CliFlag *cli__find_flag(const char *arg, CliFlag *flags) {
     for (CliFlag *f = flags; !cli_is_flag_null(f); f++) {
-        if (cli_flag_matches(arg, f)) {
+        if (cli__flag_matches(arg, f)) {
             return f;
         }
     }
+
     return NULL;
 }
 
-static void cli_add_flag(CliArgs *args, CliFlag *flag) {
-    args->parsed_flags = (CliFlag **)realloc(args->parsed_flags, sizeof(CliFlag *) * (args->parsed_flags_count + 2));
+/**
+ * Add a parsed flag to CliArgs.
+ */
+static CLIDEF void cli__add_flag(CliArgs *args, CliFlag *flag) {
     args->parsed_flags[args->parsed_flags_count++] = flag;
-    args->parsed_flags[args->parsed_flags_count] = NULL;
+    args->parsed_flags[args->parsed_flags_count]   = NULL;
 }
 
-static void cli_parse_flags(char ***argv_ptr, int *argc_ptr, CliArgs *args, CliFlag *flags) {
+/**
+ * Parse flags from argv.
+ */
+static CLIDEF void cli__parse_flags(char ***argv_ptr, int *argc_ptr, CliArgs *args, CliFlag *flags) {
     char **argv = *argv_ptr;
-    int argc = *argc_ptr;
-    if (!argv || argc <= 0 || !*argv) return;
-    char *arg = *argv;
+    int    argc = *argc_ptr;
 
+    if (!argv || argc <= 0 || !*argv) {
+        return;
+    }
+
+    char *arg = *argv;
     bool is_batch_short = arg[1] != '-' && strlen(arg) > 2;
+
     if (is_batch_short) {
         for (char *c = &arg[1]; *c; c++) {
             char sarg[3];
             snprintf(sarg, sizeof(sarg), "-%c", *c);
-            CliFlag *flag = cli_find_flag(sarg, flags);
+
+            CliFlag *flag = cli__find_flag(sarg, flags);
+
             if (!flag) {
                 fprintf(stderr, "Unknown short flag: %s\n", sarg);
                 continue;
             }
+
             flag->value = NULL;
-            cli_add_flag(args, flag);
+            cli__add_flag(args, flag);
         }
+
         return;
     }
 
-    char *eq = strchr(arg, '=');
+    char *eq    = strchr(arg, '=');
     char *value = NULL;
+
     if (eq) {
-        *eq = '\0';
+        *eq   = '\0';
         value = eq + 1;
     }
 
-    CliFlag *flag = cli_find_flag(arg, flags);
+    CliFlag *flag = cli__find_flag(arg, flags);
+
     if (!flag) {
         fprintf(stderr, "Unknown flag: %s\n", arg);
         return;
@@ -160,18 +225,23 @@ static void cli_parse_flags(char ***argv_ptr, int *argc_ptr, CliArgs *args, CliF
 
     if (!value && argc > 1 && argv[1][0] != '-') {
         value = argv[1];
+
         SHIFT_ARG(argv, argc);
     }
 
     flag->value = value;
-    cli_add_flag(args, flag);
+
+    cli__add_flag(args, flag);
 
     *argv_ptr = argv;
     *argc_ptr = argc;
 }
 
-static void cli_parse_command(const char *arg, CliArgs *args, CliCommand *commands) {
-    for (CliCommand *cmd = commands; !cli_is_command_null(cmd); cmd++) {
+/**
+ * Parse a command from argv.
+ */
+static CLIDEF void cli__parse_command(const char *arg, CliArgs *args, CliCommand *commands) {
+    for (CliCommand *cmd = commands; cmd != NULL; cmd++) {
         if (strcmp(arg, cmd->name) == 0) {
             args->command = cmd;
             return;
@@ -179,23 +249,27 @@ static void cli_parse_command(const char *arg, CliArgs *args, CliCommand *comman
     }
 }
 
-CliArgs *cli_parse_args(int argc, char *argv[], CliFlag *global_flags, CliCommand *commands) {
+/**
+ * Parse CLI arguments.
+ */
+CLIDEF CliArgs cli_parse_args(int argc, char *argv[], CliFlag *flags, CliCommand *commands) {
     assert(argc > 0);
 
-    CliArgs *args = (CliArgs *)calloc(1, sizeof(CliArgs));
-    strcpy(args->bin_name, argv[0]);
-
+    CliArgs args = {0};
+    strcpy(args.bin_name, argv[0]);
     SHIFT_ARG(argv, argc);
 
     while (argc > 0) {
         char *arg = *argv;
 
-        if (!arg) break;
+        if (!arg) {
+            break;
+        }
 
         if (arg[0] == '-') {
-            cli_parse_flags(&argv, &argc, args, global_flags);
+            cli__parse_flags(&argv, &argc, &args, flags);
         } else {
-            cli_parse_command(arg, args, commands);
+            cli__parse_command(arg, &args, commands);
         }
 
         SHIFT_ARG(argv, argc);
@@ -205,3 +279,5 @@ CliArgs *cli_parse_args(int argc, char *argv[], CliFlag *global_flags, CliComman
 }
 
 #endif // CLIARGS_IMPLEMENTATION
+
+// TODO: Add sub-commands
--- src/main.c
+++ src/main.c
@@ -13,32 +13,31 @@ static CliFlag global_flags[] = {
   { "help",    'h', NULL },
   { "version", 'v', NULL },
   { "count",   'c', NULL },
-  { NULL, 0, NULL },
+  NULL_FLAG,
 };
 
 static CliFlag download_flags[] = {
   { "url", 'u' , NULL },
-  { NULL, 0, NULL },
+  NULL_FLAG,
 };
 
 static CliCommand commands[] = {
-  { "download", download_flags },
-  { NULL, NULL },
+  { "download", {download_flags} },
+  NULL_COMMAND,
 };
 
 int main(int argc, char *argv[]) {
-  CliArgs *args = cli_parse_args(argc, argv, global_flags, commands);
+  CliArgs args = cli_parse_args(argc, argv, global_flags, commands);
+  cli_usage(args.bin_name, commands, global_flags);
 
-  usage("orbitadl", commands, global_flags);
+  printf("Binary: %s\n", args.bin_name);
 
-  printf("Binary: %s\n", args->bin_name);
-
-  if (args->command) {
-    printf("Command: %s\n", args->command->name);
+  if (args.command) {
+    printf("Command: %s\n", args.command->name);
   }
 
-  for (size_t i = 0; i < args->parsed_flags_count; i++) {
-    CliFlag *f = args->parsed_flags[i];
+  for (size_t i = 0; i < args.parsed_flags_count; i++) {
+    CliFlag *f = args.parsed_flags[i];
     printf("Flag: --%s (-%c) = %s\n", f->fullname, f->shortname, f->value ? f->value : "TRUE");
   }
 
