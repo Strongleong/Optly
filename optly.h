9ab3866162b0b2564507cd1dc40f503c9f23f1b3
Strongleong <nikita_chul@mail.ru>
2026-03-12 10:24:08 +1100

    Update doc

--- optly.h
+++ optly.h
@@ -24,27 +24,66 @@
     #define OPTLY_IMPLEMENTATION
     #include "optly.h"
 
-    static OptlyFlag flags[] = {
-      { "verbose", 'v', "Enable verbose output", {false}, OPTLY_TYPE_BOOL },
-      { "threads", 't', "Worker threads", {4}, OPTLY_TYPE_UINT32 },
-      NULL_FLAG
+    // You need to define "main" command, which is your application.
+    static OpltyCommand cmd = {
+
+      // Name will be used in usage. If you set is as NULL it will be argv[0]
+      .name = NULL,
+
+      // If not null, then it will be used in usage
+      .description = NULL,
+
+      // This is your "top-level global" flags. They should be specified before any command (./app --thread 2 -v)
+      .flags = {
+        // Long (full name)   short name   description (for usage)  Default value           Flag value  type
+        { "verbose",          'v',         "Enable verbose output", .value.as_bool = false, OPTLY_TYPE_BOOL   },
+
+        // Values are unions, so you need to specify member of value with correct type some way
+        { "threads",          't',         "Worker threads",        {.as_uint32 = 4},       OPTLY_TYPE_UINT32 },
+
+        // Flag arrays should alwasy ends with NULL_FLAG. Try to not forget about it :)
+        NULL_FLAG,
+      },
+
+      // This is your commands. (git `commit`, docker `compose` `up`)
+      .commands = {
+
+        // Instead of defining whole struct manually you can use helper functions
+        optly_command("run", "Runs server"
+
+        // optly_flags macro deals with type castings and closing array with NULL_FLAG
+        optly_flags(
+
+            // This is *command* flag. `./app -p 8080 run` will not work, but `./app run -p 8080` will
+            optly_flag_uint16("port", 'p', "Server port", .value.as_uint16 = 8080)
+        )),
+
+        // This is subcommands of command "run"
+        optly_commands(
+
+          // `./app run check` subcommand have no description, flags or subcommands
+          optly_command("check"),
+
+          // `./app run dump_config` subcommands
+          optly_command("dump_config"
+
+            // We skipped description, so we need to specify fileds now
+            .flags = optly_flags(
+              optly_flag_bool("color", 'c', "Show colors", .value.as_bool = false)
+            )
+          )
+        )
+      }
     };
 
-    static OptlyCommand commands[] = {
-      OPTLY_CMD("run",
-        { "port", 'p', "Server port", {8080}, OPTLY_TYPE_UINT16 }
-      ),
-      NULL_COMMAND
-    };
+    int main(int argc, char **argv) {
+      optly_parse_args(argc, argv, cmd);
 
-    int main(int argc, char **argv)
-    {
-      OptlyArgs args = optly_parse_args(argc, argv, flags, commands);
+      if (cmd.next_command) {
+          printf("Command: %s\n", cmd.next_command->name);
+      }
 
-      if (args.command)
-          printf("Command: %s\n", args.command->name);
-
-      printf("Threads: %u\n", flags[1].value.as_uint32);
+      printf("Threads: %u\n", cmd.flags[1].value.as_uint32);
     }
 
   Commands
