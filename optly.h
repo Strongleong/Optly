76c2cf179978e146f053bee443f898d1a357f80e
Strongleong <nikita_chul@mail.ru>
2026-04-23 11:13:16 +1100

    Versoin bumped to v2.3.1

--- optly.h
+++ optly.h
@@ -1,5 +1,5 @@
 /*
-  optly.h — v2.3.0
+  optly.h — v2.3.1
   Single-header command line argument parser for C.
 
   Features
@@ -241,7 +241,7 @@
 // Versioning macros
 #define OPTLY_VERSION_MAJOR         2
 #define OPTLY_VERSION_MINOR         3
-#define OPTLY_VERSION_RELEASE       0
+#define OPTLY_VERSION_RELEASE       1
 #define OPTLY_VERSION_NUMBER        (OPTLY_VERSION_MAJOR * 100 * 100 + OPTLY_VERSION_MINOR * 100 + OPTLY_VERSION_RELEASE)
 #define OPTLY_VERSION_FULL          OPTLY_VERSION_MAJOR.OPTLY_VERSION_MINOR.OPTLY_VERSION_RELEASE
 #define OPTLY_QUOTE(str)            #str
